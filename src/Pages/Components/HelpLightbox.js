1fafa229c5b6fec6141be977c73601ab56d10b64
FlynnTheInfinite <fabienmorival@gmail.com>
2018-06-01 10:21:52 +0200

    Responsive lightbox

--- src/Pages/Components/HelpLightbox.js
+++ src/Pages/Components/HelpLightbox.js
@@ -11,10 +11,14 @@ class HelpLightbox extends Component {
 					<div className="lightbox" onClick={e => e.stopPropagation()}>
 						<div className="lightbox-body">
 							<div className="lightbox-section">
-								<p>
+								<p className="non-handled">
 									Pour piloter la gouvernance des données personnelles de votre structure, vous aurez besoin d'un véritable chef d'orchestre
 									qui exerce une mission d'information, de conseil et de contrôle en interne : le délégué à la protection des données. Cela tombe
 									bien, on vous aide à en trouver un près de chez vous.
+								</p>
+								<p className="handled">
+									Pour gérer les données personnelles de votre structure, vous aurez besoin d'un délégué à la protection des données.
+									Trouvez-en un près de chez vous !
 								</p>
 							</div>
 							<div className="lightbox-section">
@@ -23,11 +27,17 @@ class HelpLightbox extends Component {
 								<a target="_blank" className="lightbox-link" href="https://www.cnil.fr/fr/rgpd-par-ou-commencer">Mais au fait, c'est quoi le RGPD ?</a>
 							</div>
 							<div className="lightbox-section">
-								<Button color="secondary" onClick={() => this.props.onCreateProfile()}>Je veux être référencé sur cette carte</Button>
-								<Button color="primary" onClick={() => this.props.onClose()}>Montrez-moi les DPO près de chez moi</Button>
+								<Button color="secondary" onClick={() => this.props.onCreateProfile()}>
+									<span className="non-handled">Je veux être référencé sur cette carte</span>
+									<span className="handled">S'inscrire sur la carte</span>
+								</Button>
+								<Button color="primary" onClick={() => this.props.onClose()}>
+									<span className="non-handled">Montrez-moi les DPO près de chez moi</span>
+									<span className="handled">Afficher la carte</span>
+								</Button>
 							</div>
 						</div>
-						<div className="lightbox-footer">
+						<div className="non-handled lightbox-footer">
 							<div className="big footer-box">
 								<p>
 									<b>Qui sommes-nous ?</b><br/>
@@ -38,6 +48,10 @@ class HelpLightbox extends Component {
 							<div className="little footer-box">
 								<a target="_blank" href="https://www.axeptio.eu"><Button outline color="secondary">Découvrir Axeptio</Button></a>
 							</div>
+						</div>
+						<div className="handled lightbox-footer">
+							<h6><b>Qui sommes-nous ?</b></h6>
+							<a target="_blank" href="https://www.axeptio.eu"><Button outline color="secondary">Découvrir Axeptio</Button></a>
 						</div>
 					</div>
 				</div>
