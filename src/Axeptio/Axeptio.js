80403dc09bcf68e28ac2cf7d8e4f25ee0b6d007f
FlynnTheInfinite <fabienmorival@gmail.com>
2018-06-13 09:40:52 +0200

    Updated Axeptio class

--- src/Axeptio/Axeptio.js
+++ src/Axeptio/Axeptio.js
@@ -1,78 +1,85 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class AxeptioCheckbox extends Component {
+  constructor(props) {
+    super(props);
 
-	componentDidMount () {
+    this.initialized = false;
+  }
 
-		if (!this.refs.checkbox.getAttribute("initialized")) {
+  componentDidMount() {
+    if (this.initialized) return;
 
-			window.axeptioSettings = window.axeptioSettings || {};
-			window.axeptioSettings.onChange = this.onChange(window.axeptioSettings.onChange).bind(this);
-			this.refs.checkbox.setAttribute("initialized", true);
-		}
-	}
+    window.axeptioSettings = window.axeptioSettings || {};
+    window.axeptioSettings.onChange = this.onChange(
+      window.axeptioSettings.onChange
+    ).bind(this);
+    this.initialized = true;
+  }
 
-	onChange (former) {
+  onChange(former) {
+    return (name, checked, input) => {
+      (former || new Function())(name, checked, input);
+      if (name === this.props.name)
+        (this.props.onChange || new Function())(checked, input);
+    };
+  }
 
-		return (name, checked, input) => {
-
-			(former || new Function())(name, checked, input);
-			if (name === this.props.name)
-				(this.props.onChange || new Function())(checked, input);
-		}
-	}
-
-	render () {
-
-		return (
-			<div ref="checkbox" id={"axeptio-checkbox-" + this.props.name.replace(/ /g,'')} className="axeptio-checkbox">
-				<input type="checkbox" name={this.props.name} data-axeptio={this.props.data}/>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="axeptio-checkbox">
+        <input
+          type="checkbox"
+          name={this.props.name}
+          data-axeptio={this.props.data}
+        />
+      </div>
+    );
+  }
 }
 
 export default class Axeptio extends Component {
+  constructor(props) {
+    super(props);
 
-	onChange = former => (name, checked, input) => {
+    this.initialized = false;
+  }
 
-		(former || new Function())(name, checked, input);
-		(this.props.onChange || new Function())(name, checked, input);
-	}
+  onChange(former) {
+    return (name, checked, input) => {
+      (former || new Function())(name, checked, input);
+      (this.props.onChange || new Function())(name, checked, input);
+    };
+  }
 
-	componentDidMount() {
+  componentDidMount() {
+    if (this.initialized || document.getElementById("axeptio-script")) return;
 
-		if (!document.getElementById("axeptio-script")) {
+    window.axeptioSettings = window.axeptioSettings || {};
+    window.axeptioSettings = {
+      clientId: this.props.clientId || null,
+      token: this.props.token || null,
+      onToken: this.props.onToken || new Function(),
+      onChange: this.onChange(window.axeptioSettings.onChange).bind(this),
+      initAtLoad: this.props.initAtLoad || true,
+      globalPrototypeName: this.props.globalPrototypeName || "Axeptio",
+      globalInstanceName: this.props.globalInstanceName || "Axeptio",
+      debug: this.props.debug || false,
+      axeptioApiUrl: this.props.axeptioApiUrl || "https://api.axept.io/v1",
+      axeptioPlatformUrl:
+        this.props.axeptioPlatformUrl || "https://platform.axept.io"
+    };
 
-			window.axeptioSettings = window.axeptioSettings || {};
-	        window.axeptioSettings = {
-	          clientId: this.props.clientId || null,
-	          token: this.props.token || null,
-	          onToken: this.props.onToken || new Function(),
-	          onChange:this.onChange(window.axeptioSettings.onChange).bind(this),
-	          initAtLoad: this.props.initAtLoad || true,
-	          globalPrototypeName: this.props.globalPrototypeName || "Axeptio",
-	          globalInstanceName: this.props.globalInstanceName || "Axeptio",
-	          debug: this.props.debug || false,
-	          axeptioApiUrl: this.props.axeptioApiUrl || "https://api.axept.io/v1",
-	          axeptioPlatformUrl: this.props.axeptioPlatformUrl || "https://platform.axept.io"
-	        };
+    const script = document.createElement("script");
+    script.setAttribute("id", "axeptio-script");
 
-	        const script = document.createElement("script");
-	        script.setAttribute("id", "axeptio-script");
+    script.src = "https://platform.axept.io/embed.js";
+    this.initialized = true;
 
-	        script.src = "https://platform.axept.io/embed.js";
+    document.body.appendChild(script);
+  }
 
-			document.body.appendChild(script);
-		}
-    }
-
-	render () {
-
-		return (
-			<div className="axeptio">
-				{ this.props.children }
-			</div>
-		)
-	}
+  render() {
+    return <div className="axeptio">{this.props.children}</div>;
+  }
 }
